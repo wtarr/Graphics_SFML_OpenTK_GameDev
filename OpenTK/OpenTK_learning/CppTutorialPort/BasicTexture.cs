208943fb882262fd27167046c540d30ea8541b57
William <william.projects.2012@gmail.com>
2014-06-02 23:24:36 +0100

    Coloured triangle and basic texturing

--- OpenTK/OpenTK_learning/CppTutorialPort/BasicTexture.cs
+++ OpenTK/OpenTK_learning/CppTutorialPort/BasicTexture.cs
@@ -1,43 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Windows.Forms;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 
 namespace CppTutorialPort
 {
-    /// <summary>
-    /// Cpp to C# port
-    /// Based on Open.gl tutorial
-    /// http://open.gl/content/code/c2_color_triangle.txt
-    /// </summary>
-    class TriangleColor : GameWindow, IExample
+    class BasicTexture : GameWindow, IExample
     {
-       private string vertexSource = @"
+        private string vertexSource = @"
 #version 150 core
-
 in vec2 position;
 in vec3 color;
-
+in vec2 texcoord;
 out vec3 Color;
-
+out vec2 Texcoord;
 void main() {
 Color = color;
+Texcoord = texcoord;
 gl_Position = vec4(position, 0.0, 1.0);
 }";
 
         private string fragmentSource = @"
 #version 150 core
 in vec3 Color;
+in vec2 Texcoord;
 out vec4 outColor;
-
+uniform sampler2D tex;
 void main() {
-outColor = vec4(Color, 1.0);
+outColor = texture(tex, Texcoord) * vec4(Color, 1.0);
 }";
 
         private int vertexShader,
@@ -45,18 +41,31 @@ outColor = vec4(Color, 1.0);
             shaderProgram,
             vao,
             vbo,
+            ebo,
             posAttrib,
-            colAttrib;
+            colAttrib,
+            texAttrib,
+            tex,
+            width,
+            height;
 
         private float[] vertices =
         {
-         0.0f,  0.5f, 1.0f, 0.0f, 0.0f,
-         0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f
+            // position   //Color           //Texcoords
+            -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Top-left
+             0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Top-right
+             0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // Bottom-right
+            -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f  // Bottom-left
+        };
+
+        private int[] elements =
+        {
+            0, 1, 2,
+            2, 3, 0
         };
         
-        public TriangleColor()
-            : base(640, 480, new GraphicsMode(), "Basic Hello World Triangle Port",
+        public BasicTexture()
+            : base(800, 600, new GraphicsMode(), "Basic Texture Port",
                 0, DisplayDevice.Default, 3, 2, GraphicsContextFlags.ForwardCompatible | GraphicsContextFlags.Debug)
         {
 
@@ -70,13 +79,20 @@ outColor = vec4(Color, 1.0);
 
             // crate vertex buffer object
             vbo = GL.GenBuffer();
-            
+
             GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
             GL.BufferData(BufferTarget.ArrayBuffer,
                 new IntPtr(vertices.Length * sizeof(float)),
                 vertices, BufferUsageHint.StaticDraw);
 
-            // create and compile the vertex shader
+            // Create an element array
+            ebo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+            GL.BufferData(BufferTarget.ElementArrayBuffer,
+                new IntPtr(elements.Length * sizeof(int)),
+                elements, BufferUsageHint.StaticDraw);
+
+            // create shader
             vertexShader = GL.CreateShader(ShaderType.VertexShader);
             GL.ShaderSource(vertexShader, vertexSource);
             GL.CompileShader(vertexShader);
@@ -104,15 +120,36 @@ outColor = vec4(Color, 1.0);
             // specify the layout of the vertex data
             posAttrib = GL.GetAttribLocation(shaderProgram, "position");
             GL.EnableVertexAttribArray(posAttrib);
-            GL.VertexAttribPointer(posAttrib, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
-
-            
-
+            GL.VertexAttribPointer(posAttrib, 2, VertexAttribPointerType.Float, false, 
+                7 * sizeof(float), 0);
 
             colAttrib = GL.GetAttribLocation(shaderProgram, "color");
             GL.EnableVertexAttribArray(colAttrib);
-            GL.VertexAttribPointer(colAttrib, 3, VertexAttribPointerType.Float, false,
-                5 * sizeof(float), new IntPtr(2 * sizeof(float)));
+            GL.VertexAttribPointer(colAttrib, 3, VertexAttribPointerType.Float, 
+                false, 7 * sizeof(float),
+                new IntPtr(2 * sizeof(float)));
+
+            texAttrib = GL.GetAttribLocation(shaderProgram, "texcoord");
+            GL.EnableVertexAttribArray(texAttrib);
+            GL.VertexAttribPointer(texAttrib, 2, VertexAttribPointerType.Float,
+                false, 7 * sizeof(float), new IntPtr(5 * sizeof(float)));
+
+            // Load texture
+            tex = GL.GenTexture();
+
+            Bitmap bmp = new Bitmap("Resources//cat.jpg");
+            BitmapData bmp_data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, bmp_data.Width, bmp_data.Height, 0,
+                OpenTK.Graphics.OpenGL.PixelFormat.Rgba, PixelType.UnsignedByte, bmp_data.Scan0);
+
+            bmp.UnlockBits(bmp_data);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)All.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)All.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)All.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)All.Linear);
+            
 
 
         }
@@ -134,12 +171,14 @@ outColor = vec4(Color, 1.0);
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
-            GL.ClearColor(System.Drawing.Color.Green);
+            GL.ClearColor(System.Drawing.Color.Beige);
             GL.Clear(ClearBufferMask.ColorBufferBit);
-
+            
             // Draw a triangle from the 3 vertices
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
-            GL.Flush();
+            //GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+            GL.DrawElements(PrimitiveType.Triangles, elements.Length,
+                DrawElementsType.UnsignedInt, IntPtr.Zero);
+
             SwapBuffers();
         }
         private void CleanUp()
